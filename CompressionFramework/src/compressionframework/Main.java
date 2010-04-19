9162bc2018bb4fd2268969045803907640fbfcd6
jjanduy <jjanduy@c1fe942b-0e7e-3928-87a8-756bc00401f2>
2010-04-19 02:58:22 +0000

    Arquitetura do projeto

---
+++ CompressionFramework/src/compressionframework/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package compressionframework;
+
+/**
+ *
+ * @author Janduy
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
