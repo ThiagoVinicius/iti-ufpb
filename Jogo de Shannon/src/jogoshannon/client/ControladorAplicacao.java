a391e882e2f8ef048b1159e68b0106539c8a5a19
Thiagovfar <Thiagovfar@c1fe942b-0e7e-3928-87a8-756bc00401f2>
2010-06-16 02:01:24 +0000

    Já temos uma interface (sebosinha).

---
+++ Jogo de Shannon/src/jogoshannon/client/ControladorAplicacao.java
@@ -0,0 +1,57 @@
+package jogoshannon.client;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.HasWidgets;
+
+import jogoshannon.client.presenter.Apresentador;
+import jogoshannon.client.presenter.PrincipalApresentador;
+import jogoshannon.client.view.PrincipalExibicao;
+
+public class ControladorAplicacao implements Apresentador, ValueChangeHandler<String> {
+
+	private HandlerManager eventos;
+	private HasWidgets pagina;
+	
+	public ControladorAplicacao (HandlerManager eventos) {
+		this.eventos = eventos;
+		amarra();
+	}
+	
+	private void amarra() {
+		History.addValueChangeHandler(this);
+	}
+	
+	@Override
+	public void vai(HasWidgets pagina) {
+		this.pagina = pagina;
+		
+		if ("".equals(History.getToken())) {
+			History.newItem("jogar");
+		} else {
+			History.fireCurrentHistoryState();
+		}
+	}
+
+	@Override
+	public void onValueChange(ValueChangeEvent<String> event) {
+		String token = event.getValue();
+		
+		if (token != null) {
+			Apresentador oEscolhido = null;
+			
+			if (token.equals("jogar")) {
+				oEscolhido = new PrincipalApresentador(eventos, new PrincipalExibicao());
+			}
+			
+			if (oEscolhido != null) {
+				oEscolhido.vai(pagina);
+			}
+			
+		}
+		
+	}
+
+}
---
+++ Jogo de Shannon/src/jogoshannon/client/view/PrincipalExibicao.java
@@ -0,0 +1,76 @@
+package jogoshannon.client.view;
+
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import jogoshannon.client.presenter.PrincipalApresentador;
+
+public class PrincipalExibicao extends Composite implements PrincipalApresentador.Exibicao {
+	
+	private Button botaoEnviar;
+	private Label labelFrase;
+	private TextBox textResposta;
+	private Label labelErro;
+	
+	public PrincipalExibicao () {
+		DecoratorPanel root = new DecoratorPanel();
+		initWidget(root);
+		
+		VerticalPanel subRoot = new VerticalPanel();
+		
+		HorizontalPanel painel = new HorizontalPanel();
+		painel.setBorderWidth(0);
+		painel.setHorizontalAlignment(HorizontalPanel.ALIGN_LEFT);
+		painel.setSpacing(10);
+		labelFrase = new Label();
+		labelFrase.setWidth("20em");
+		botaoEnviar = new Button("Ok");
+		textResposta = new TextBox();
+		textResposta.setMaxLength(1);
+		textResposta.setWidth("1em");
+		painel.add(labelFrase);
+		painel.add(textResposta);
+		painel.add(botaoEnviar);
+		
+		labelErro = new Label();
+		
+		subRoot.add(painel);
+		subRoot.add(labelErro);
+		
+		root.add(subRoot);
+		
+	}
+	
+	@Override
+	public HasClickHandlers getBotaoEnviar() {
+		return botaoEnviar;
+	}
+
+	@Override
+	public char getResposta() {
+		return textResposta.getText().charAt(0);
+	}
+
+	@Override
+	public void setDesafio(String frase) {
+		textResposta.setFocus(true);
+		labelFrase.setText(frase);
+	}
+
+	@Override
+	public void setTextoErro(String erro) {
+		labelErro.setText(erro);
+	}
+	
+	public Widget asWidget () {
+		return this;
+	}
+
+}
