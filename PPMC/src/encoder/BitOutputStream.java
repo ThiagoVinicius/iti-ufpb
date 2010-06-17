de76b09dc635168a4ab3d661cdf7af9b023f5f15
jjanduy <jjanduy@c1fe942b-0e7e-3928-87a8-756bc00401f2>
2010-06-17 03:03:47 +0000

    Corrigido erros.

--- PPMC/src/encoder/BitOutputStream.java
+++ PPMC/src/encoder/BitOutputStream.java
@@ -5,58 +5,64 @@ import java.io.DataOutputStream;
 
 public final class BitOutputStream {
 
-	DataOutputStream outputFinal;
+    static final int CODE_VALUE_BITS = 31;
+    DataOutputStream outputFinal;
+    ByteArrayOutputStream outputTemp;
+    int buffer = 0;
+    int bitsNoBuffer = 0;
 
-	ByteArrayOutputStream outputTemp;
+    public BitOutputStream(DataOutputStream out) throws Exception {
+        outputFinal = out;
+        outputTemp = new ByteArrayOutputStream();
+    }
 
-	int buffer = 0;
-	int bitsNoBuffer = 0;
+    public int flush() throws Exception {
 
-	public BitOutputStream(DataOutputStream out)throws Exception{
-		outputFinal = out;
-		outputTemp = new ByteArrayOutputStream();
-	}
+        for(int i = 0; i < CODE_VALUE_BITS-2; i++)
+        {
+            writeBitFalse();
+        }
 
-	public int flush()throws Exception{
-		while(bitsNoBuffer < 8)
-			writeBitFalse();//salvar 0 no final ate completar 8 bits
-		esvaziarBuffer();
+        while (bitsNoBuffer < 8) {
+            writeBitFalse();//salvar 0 no final ate completar 8 bits
+        }
+        esvaziarBuffer();
 
-		//fechar temp
-		outputTemp.flush();
-		outputTemp.close();
+        //fechar temp
+        outputTemp.flush();
+        outputTemp.close();
 
-		//copiar temp pro outputFinal
-		byte[] temp = outputTemp.toByteArray();
-		outputFinal.writeInt(temp.length);//salvar quantos bytes escritos
-		outputFinal.write(temp);
-		return temp.length;	
-	}
+        //copiar temp pro outputFinal
+        //byte[] temp = outputTemp.toByteArray();
+        //outputFinal.writeInt(temp.length);//salvar quantos bytes escritos
+        //outputFinal.write(temp);
+        return outputFinal.size();
+    }
 
-	public void writeBitTrue()throws Exception{
-		if(bitsNoBuffer == 8){
-			esvaziarBuffer();
-			writeBitTrue();
-		}else{
-			buffer = (buffer << 1) | 1;//add 1
-			bitsNoBuffer++;
-		}
-	}
+    public void writeBitTrue() throws Exception {
+        if (bitsNoBuffer == 8) {
+            esvaziarBuffer();
+            writeBitTrue();
+        } else {
+            buffer = (buffer << 1) | 1;//add 1
+            bitsNoBuffer++;
+        }
+    }
 
-	public void writeBitFalse()throws Exception{
-		if(bitsNoBuffer == 8){
-			esvaziarBuffer();
-			writeBitFalse();
-		}else{
-			buffer = buffer << 1;//add 0
-			bitsNoBuffer++;
-		}
-	}
+    public void writeBitFalse() throws Exception {
+        if (bitsNoBuffer == 8) {
+            esvaziarBuffer();
+            writeBitFalse();
+        } else {
+            buffer = buffer << 1;//add 0
+            bitsNoBuffer++;
+        }
+    }
 
-	private void esvaziarBuffer()throws Exception{
-		outputTemp.write(buffer);
-		buffer = 0;
-		bitsNoBuffer = 0;
-	}
-
+    private void esvaziarBuffer() throws Exception {
+        outputFinal.write(buffer);
+        outputFinal.flush();
+        buffer = 0;
+        bitsNoBuffer = 0;
+    }
 }
