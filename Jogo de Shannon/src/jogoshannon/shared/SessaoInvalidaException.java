20d9530384670e35cafca686e32e46acee377334
Thiagovfar <Thiagovfar@c1fe942b-0e7e-3928-87a8-756bc00401f2>
2010-07-17 22:34:34 +0000

    Finalizando refatoracao e remocao de warnings.

--- Jogo de Shannon/src/jogoshannon/shared/SessaoInvalidaException.java
+++ Jogo de Shannon/src/jogoshannon/shared/SessaoInvalidaException.java
@@ -2,26 +2,23 @@ package jogoshannon.shared;
 
 import java.io.Serializable;
 
+@SuppressWarnings("serial")
 public class SessaoInvalidaException extends Exception implements Serializable {
 
     public SessaoInvalidaException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public SessaoInvalidaException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 
     public SessaoInvalidaException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public SessaoInvalidaException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 
 }
